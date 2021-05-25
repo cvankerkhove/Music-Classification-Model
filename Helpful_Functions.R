cc44a68c6564d38aef5e1686d0465e8eee3867b8
Chris VanKerkhove <cjv47@cornell.edu>
2021-05-24 20:21:13 -0400

    Random Forrest
    Adding Random Forest as a method.

--- Helpful_Functions.R
+++ Helpful_Functions.R
@@ -33,15 +33,15 @@ run.knn <- function(df, K, labels, train.pct) {
   return(list(t, error))
 }
 
-run.knn.cv <- function(df, K, labels, n.folds, n.iterations) {
+run.knn.cv <- function(df, K, labels, k.folds, n.iterations) {
   #This function runs KNN algorithm on a valid
   #music data frame input and outputs a list of an 
   #output table and the KNN classification error
   #using the validation set approach
   #Arg(s):
-  #k: k paramter for knn algorithm
+  #k: K paramter for knn algorithm
   #labels: genres of music to use in classification
-  #n.folds: number of folds in the cross-validation
+  #k.folds: number of folds in the cross-validation
   #n.iterations: number of iterations of perfroming cross-validation to get error
   
   #filtering on selected labels
@@ -56,10 +56,10 @@ run.knn.cv <- function(df, K, labels, n.folds, n.iterations) {
     #classification error from each fold
     cv.error <- c()
     indices <- sample(nrow(df2), nrow(df2))
-    for (i in 1:n.folds) {
+    for (i in 1:k.folds) {
       #testing indices for this fold
-      slice1 <- (i-1)*(nrow(df2)/n.folds)+1
-      slice2 <- i*(nrow(df2)/n.folds)
+      slice1 <- (i-1)*(nrow(df2)/k.folds)+1
+      slice2 <- i*(nrow(df2)/k.folds)
       test_ind <- indices[slice1:slice2]
       df.train <- df.normal[-test_ind,]                    
       df.test <- df.normal[test_ind,]
@@ -76,3 +76,73 @@ run.knn.cv <- function(df, K, labels, n.folds, n.iterations) {
   }
   return(mean(errors))
 }
+
+
+run.random.forest.cv <- function(df, mtry, k.folds, n.iterations, single.genre=NULL) {
+  #this function runs and creates and performs cross validation on a dataset 
+  #and returns the overall cv classification error on this dataset withh specifications
+  #for number of folds in the cv and number of iterations to perform
+  #Arg(s):
+    #df: a valid dataframe adapted from original music.df
+    #mtry: the mtry paramater in the random forest
+    #k.folds: number of folds in the cross validation test
+    #n.iterations: number of times to perform the cross-validation
+  
+  #overall error for each trial
+  errors <- c()
+  #errors dataframe for each genre n x n.iterations matrix (n is number of genres)
+  genre.errors <- NULL
+  for (itr in 1:n.iterations) {
+    #sampling new other data for each iteration
+    if (!(is.null(single.genre))) {
+      df.genre <- filter(df, !(label == single.genre)) %>%
+        sample_n(100, replace = FALSE) %>%
+        mutate(label = 'other') %>%
+        bind_rows(filter(df.tree, label == 'rock'))
+      df.genre$label <- as.factor(df.genre$label)
+      df <- df.genre
+    }
+    #classification error from each fold
+    cv.errors <- c()
+    #classification error for each genre for each fold
+    cv.genre.errors <- data.frame()
+    #indices to split data into for cross validation
+    indices <- sample(nrow(df), nrow(df))
+    for (i in 1:k.folds) {
+      #testing indices for this fold
+      slice1 <- (i-1)*(nrow(df)/k.folds)+1
+      slice2 <- i*(nrow(df)/k.folds)
+      test_ind <- indices[slice1:slice2]
+      df.train <- df[-test_ind,]                    
+      df.test <- df[test_ind,]
+      #Create a Random Forest
+      rf <- randomForest(label~., data=df.train, mtry=mtry, importance=TRUE)
+      pred <- predict(rf, newdata=df.test)
+      genres <- rf$classes
+      #overall classification error
+      cv.errors[i] <- 1 - mean(pred == df.test$label)
+      #classification error for each genre
+      for (g in 1:length(genres)) {
+        #true positive accuracy
+        ind <- which(df.test$label == genres[g])
+        error <- 1 - mean(df.test$label[ind] == pred[ind])
+        #updating cv.genre.errors matrix
+        cv.genre.errors[i, genres[g]] <- error
+      }
+    }
+    #overall classification error
+    errors[itr] <- mean(cv.errors)
+    #classification error by genre
+    if (is.null(genre.errors)) {
+      genre.errors <- sapply(cv.genre.errors, FUN=mean)
+    }
+    else {
+      genre.errors <- bind_rows(genre.errors, sapply(cv.genre.errors, FUN=mean))
+    }
+  }
+  #final return values for n.iterations of n.fold cross validation
+  overall.errors <- mean(errors)
+  overall.genre.errors <- sapply(genre.errors, FUN = mean)
+  return (list(overall.errors, overall.genre.errors))
+}
+
--- sandbox.R
+++ sandbox.R
@@ -2,6 +2,7 @@ setwd("~/Desktop/ORIE 4740/Final Project")
 library(readr)
 library(tibble)
 library(dplyr)
+library(ggplot2)
 source('Helpful_Functions.R')
 
 ####Data Cleaning and Manipulation###
@@ -43,9 +44,24 @@ mean(pred.bin == actual.bin)
 
 
 
-
-
 ####K-Nearest-Neighbors####
+#running multinomial classification for different values of K
+#computing error through 10-fold CV 10 times
+k.values <- seq(1,30,1)
+errors <- c()
+cv.errors <- c()
+for (i in 1:length(k.values)) {
+  #errors from validation set approach
+  out <- run.knn(df, k.values[i], levels(df$label), 0.6)
+  errors[i] <- out[[2]]
+  #errors from 10-fold, 10 iteration CV
+  out2 <- run.knn.cv(df, k.values[i], levels(df$label), 10, 10)
+  cv.errors[i] <- out2
+}
+plot(k.values, errors)
+plot(k.values, cv.errors)
+
+
 #computing error classification error by each class
 genres <- levels(df$label)
 #dataframe to store error data for each genre across iterations
@@ -70,25 +86,19 @@ for (itr in 1:n) {
 }
 
 errors <- summarise_each(errors.df, list(.=mean))
-errors
-barplot(as.matrix(errors), las=2)
-
-
-#running multinomial classification for different values of K
-#computing error through 10-fold CV 10 times
-k.values <- seq(1,30,1)
-errors <- c()
-cv.errors <- c()
-for (i in 1:length(k.values)) {
-  #errors from validation set approach
-  out <- run.knn(df, k.values[i], levels(df$label), 0.6)
-  errors[i] <- out[[2]]
-  #errors from 10-fold, 10 iteration CV
-  out2 <- run.knn.cv(df, k.values[i], levels(df$label), 10, 10)
-  cv.errors[i] <- out2
+##ggplot2 barplotting
+#getting error data in a valid input to ggplot2
+errors.num <- c()
+for (i in 1:ncol(errors)) {
+  errors.num[genres[i]] <- errors[[i]]
 }
-plot(k.values, errors)
-plot(k.values, cv.errors)
+
+errors.df <- cbind(genre = rownames(as.data.frame(errors.num)), as.data.frame(errors.num))
+rownames(errors.df) <- 1:nrow(errors.df)
+#plotting
+ggplot(errors.df, aes(genre, errors.num)) +
+  geom_bar(stat = "identity")
+
 
 
 #binary classification for each genre
@@ -97,7 +107,9 @@ plot(k.values, cv.errors)
 #select 100 random data points from non-genre and label as 'other'
 #run KNN 
 genres <- levels(df$label)
-error <- c()
+#list for recording errors of KNN
+errors <- c()
+#lists for finding best k-value for each genre
 k.values <- seq(1,20,1)
 best.k <- c()
 
@@ -113,16 +125,146 @@ for (i in 1:length(genres)) {
   #tables[[genres[i]]] <- out[[1]]
   #list for storing error values of each K 
   k.list <- c()
-  for (k in 2:length(k.values)) {
+  for (k in 1:length(k.values)) {
     out <- run.knn.cv(df.genre, k.values[k], levels(df.genre$label), 10, 10)
     k.list[k] <- out
   }
-  print(k.list)
   best.k[genres[i]] <- which.min(k.list)
-  error[genres[i]] <- min(k.list)
+  errors[genres[i]] <- min(k.list)
 }
 
-error
-best.k
+##ggplot2 barplotting
+#getting error data in a valid input to ggplot2
+errors.df <- cbind(genre = rownames(as.data.frame(errors)), as.data.frame(errors),
+                        as.data.frame(best.k))
+rownames(errors.df) <- 1:nrow(errors.df)
+errors.df$best.k <- as.factor(errors.df$best.k)
+
+ggplot(errors.df, aes(genre, errors, fill=best.k)) +
+  geom_bar(stat = "identity")
+
+
+
+
+####Decision Trees####
+library(tree)
+df.tree <- df
+tree.music <- tree(label~., df.tree)
+summary(tree.music)
+plot(tree.music)
+text(tree.music, pretty=0)
+cv1 <- cv.tree(tree.music, FUN = prune.misclass)
+
+#validation set
+train <- sample(1:1000, 700, replace=FALSE)
+tree.music <- tree(label~., df.tree, subset=train)
+tree.pred <- predict(tree.music, df.tree[-train,], type="class")
+t1 <- table(tree.pred, df.tree[-train,]$label)
+#using best cv
+pruned.tree.music <- prune.misclass(tree.music, best = cv1$size[which.min(cv1$dev)])
+tree.pred2 <- predict(pruned.tree.music, df.tree[-train,], type='class')
+t2 <- table(tree.pred2, df.tree[-train,]$label )
+#Getting each genre classification error
+genres <- levels(df.tree$label)
+errors <- c()
+pruned.errors <- c()
+for (i in 1:length(genres)) {
+  print(genres[i])
+  num1 <- t1[genres[i], genres[i]]
+  num2 <- t2[genres[i], genres[i]]
+  denom1 <- 0
+  denom2 <- 0
+  for (i2 in 1:nrow(t1)) {
+    denom1 <- denom1 + t1[genres[i], i2]
+    denom2 <- denom2 + t2[genres[i], i2]
+  }
+  errors[genres[i]] <- 1 - (num1 / denom1)
+  pruned.errors[genres[i]] <- 1 - (num2/ denom2)
+}
+errors
+pruned.errors
+mean(pruned.errors)
+
+#plotting errors by genre
+##ggplot2 barplotting
+#getting error data in a valid input to ggplot2
+errors.df <- cbind(genre = rownames(as.data.frame(errors)), as.data.frame(errors))
+rownames(errors.df) <- 1:nrow(errors.df)
+
+ggplot(errors.df, aes(genre, errors)) +
+  geom_bar(stat = "identity")
+
+##Binomial Classification
+#single genre
+#Comparing standard tree vs pruned tree prediction
+df.genre <- filter(df.tree, !(label == 'rock')) %>%
+  sample_n(100, replace = FALSE) %>%
+  mutate(label = 'other') %>%
+  bind_rows(filter(df, label == 'rock'))
+df.genre$label <- as.factor(df.genre$label)
+tree.1genre <- tree(label~., df.genre)
+cv1 <- cv.tree(tree.1genre, FUN = prune.misclass)
+#plottin deviation vs size of tree
+plot(cv1$size, cv1$dev, type='b')  
+#pruned tree prediction
+pruned.tree <- prune.misclass(tree.1genre, best=cv1$size[which.min(cv1$dev)])
+pruned.tree2 <- prune.misclass(tree.1genre, k = cv1$k[which.min(cv1$dev)])
+pred1 <- predict(pruned.tree, newdata=df.genre, type='class')
+pred2 <- predict(pruned.tree, newdata=df.genre, type='class')
+table(pred1, df.genre$label)
+table(pred2, df.genre$label)
+
+#Iterating through all Genres
+genres <- levels(df.tree$label)
+errors <- c()
+for (i in 1:length(genres)) {
+  #modifying main dataset to be a balanced 200 rows each time
+  df.genre <- filter(df.tree, !(label == genres[i])) %>%
+    sample_n(100, replace = FALSE) %>%
+    mutate(label = 'other') %>%
+    bind_rows(filter(df.tree, label == genres[i]))
+  df.genre$label <- as.factor(df.genre$label)
+  #Creating a tree and pruned tree using training data
+  train <- sample(1:nrow(df.genre), nrow(df.genre)*0.70)
+  tree.1genre <- tree(label~., df.genre[train,])
+  cv1 <- cv.tree(tree.1genre, FUN = prune.misclass)
+  pruned.tree <- prune.misclass(tree.1genre, best=cv1$size[which.min(cv1$dev)])
+  #predictions train
+  pred <- predict(pruned.tree, newdata=df.genre[-train,], type='class')
+  t <-table(pred, df.genre[-train,]$label)
+  errors[genres[i]] <-  1 - mean(pred == df.genre[-train,]$label)
+}
+
+
+
+#####Random Forest####
+library(randomForest)
+##Use bagging and randomforest with these decision trees p=10
+rf.tree1 <- randomForest(label~., data=df.tree, mtry=10, importance=TRUE)
+
+set.seed(18167)
+df.genre <- filter(df.tree, (label == 'rock')) %>%
+  bind_rows(filter(df, label == 'blues'))
+df.genre$label <- as.factor(as.character(df.genre$label))
+
+
+df.genre <- filter(df.tree, !(label == 'rock')) %>%
+  sample_n(100, replace = FALSE) %>%
+  mutate(label = 'other') %>%
+  bind_rows(filter(df.tree, label == 'rock'))
+df.genre$label <- as.factor(df.genre$label)
+
+#mtry parameter tuning
+errors <- c()
+genre.errors <- c()
+mtry <- seq(1,15,1)
+for (i in 1:length(mtry)) {
+  print(i)
+  rf.cv <- run.random.forest.cv(df, mtry = mtry[i], 10, 10, 'rock')
+  errors[i] <- rf.cv[[1]]
+  #genre.errors[i] <- rf.cv[[2]]
+}
+plot(mtry, errors)
+
 
 
